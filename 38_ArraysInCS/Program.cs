58ff111485c4f294ff2386a827aa23faa1609bbe
dmitry.ivanov@hubsecurity.io <dmitry.ivanov@hubsecurity.io>
2024-11-21 17:48:40 +0200

    38_ArraysInCS

---
+++ 38_ArraysInCS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_ArraysInCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 111, 10, 4, 99, 49, 64, 77, 4, 42, 5 };
+            int result = myArray.Max();
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
