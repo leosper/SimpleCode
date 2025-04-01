912cd3bf2d83ba817c5613372ae1edfb804e1818
mitry <leosper@protonmail.com>
2025-04-01 18:26:49 +0300

    39

---
+++ 39_ArraysWorkWith/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_ArraysWorkWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 111, 10, 4, 99, 49, 64, 77, 4, 42, 5 };
+            Console.WriteLine(myArray.Where(i => i % 2 != 0).Min());
+            Console.ReadLine();
+            int[] result = myArray.Distinct().ToArray();
+
+            Array.Find(result, i => i < 70);
+            Array.FindLast(result, i => i < 70);
+
+            Array.FindIndex;
+            Array.FindLastIndex;
+
+        }
+    }
+}
